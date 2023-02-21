d38d6d01055f3f39d5feb166bc5d5d4a15bdd06c
xFarisSx <faristaleek@gmail.com>
2023-02-21 22:47:52 +0300

    Circles

--- script.js
+++ script.js
@@ -5,43 +5,32 @@ canvas.height = 800;
 
 // Global settings
 ctx.lineWidth = 10
+ctx.shadowColor = 'white'
 ctx.shadowOffsetX = 2
 ctx.shadowOffsetY = 2
-ctx.shadowColor = 'White'
 
-const gradient1 = ctx.createLinearGradient(0,0,canvas.width, canvas.height)
-gradient1.addColorStop('0.2', 'pink')
-gradient1.addColorStop('0.3', 'red')
-gradient1.addColorStop('0.4', 'orange')
-gradient1.addColorStop('0.5', 'yellow')
-gradient1.addColorStop('0.6', 'green')
-gradient1.addColorStop('0.7', 'turquoise')
-gradient1.addColorStop('0.8', 'violet')
-const gradient2 = ctx.createRadialGradient(canvas.width * 0.5, canvas.height *0.5, 70, canvas.width * 0.5, canvas.height *0.5, 400)
-gradient2.addColorStop('0.4', 'red')
-gradient2.addColorStop('0.6', 'orange')
-
-
-const patternImage = document.getElementById('patternImage')
-const pattern1 = ctx.createPattern(patternImage, 'no-repeat')
-
-ctx.strokeStyle = pattern1
+ctx.strokeStyle = 'red'
 class Line {
     constructor(canvas){
         this.canvas = canvas
         this.x = Math.random() * this.canvas.width
         this.y = Math.random() * this.canvas.height
         this.history = [{x: this.x, y: this.y}]
-        this.lineWidth = Math.floor(Math.random() * 15 + 1)
+        this.lineWidth = Math.floor(Math.random() * 25 + 1)
         this.hue = Math.floor(Math.random() * 360)
         this.maxLength = Math.floor(Math.random()*150 + 10)
         this.speedX = Math.random() * 1 - 0.5
         this.speedY = 7
         this.lifeSpan = this.maxLength * 3
+        this.breakPoint = this.lifeSpan * 0.85
         this.timer = 0
+        this.angle = 0
+        this.va = Math.random() * 0.5 - 0.25
+        this.curve = 0.1
+        this.vc = Math.random() * 0.4 - 0.2
     }
     draw(context) {
-        // context.strokeStyle = `hsl(${this.hue}, 100%, 50%)`
+        context.strokeStyle = `hsl(${this.hue}, 100%, 50%)`
         context.lineWidth = this.lineWidth
         context.beginPath()
         context.moveTo(this.history[0].x, this.history[0].y)
@@ -52,10 +41,15 @@ class Line {
     }
 
     update(){
+        this.angle+=this.va
         this.timer++
+        this.curve+=this.vc
         if (this.timer < this.lifeSpan){
-            this.x += this.speedX + Math.random() * 20 -10
-            this.y += this.speedY +  Math.random() * 20 -10
+            if(this.timer > this.breakPoint) {
+                this.va*= -1.12
+            }
+            this.x += Math.sin(this.angle) * this.curve
+            this.y +=  Math.cos(this.angle) * this.curve
             this.history.push({x: this.x, y: this.y})
             if (this.history.length > this.maxLength) {
                 this.history.shift()
@@ -72,11 +66,14 @@ class Line {
         this.y = Math.random() * this.canvas.height
         this.history = [{x: this.x, y: this.y}]
         this.timer = 0
+        this.angle = 0
+        this.curve = 0
+        this.va = Math.random() * 0.5 - 0.25
     }
 }
 
 const linesArray = []
-const numberOfLines = 200;
+const numberOfLines = 50;
 for (let i = 0; i < numberOfLines ; i++) {
     linesArray.push(new Line(canvas))
 }
